d01b4ab2f1204a99d74f6ee342916666709a5ea9
Max Samsonenko <samsonenko.maxwork@gmail.com>
2024-03-09 19:43:07 +0200

    complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Observer } from "./homework/1";
+import RequestComponent from "./homework/2";
+import { FormComponent } from "./homework/3";
+import { ComponentApp } from "./homework/4";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const handleContentEndVisible = () => {
+		console.log("Reached the end of the content!");
+	};
+	return (
+		<div className="App">
+			<Observer onContentEndVisible={handleContentEndVisible}>
+				<RequestComponent />
+				<FormComponent />
+				<ComponentApp />
+			</Observer>
+		</div>
+	);
 }
 
 export default App;
